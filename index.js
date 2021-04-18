3dd182f3204525b2a33ab8778959593ee8e7da57
Anant8002749 <ak8002749@gmail.com>
2021-04-18 11:01:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const port = 8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('Server is Running on port:',port);
+})
