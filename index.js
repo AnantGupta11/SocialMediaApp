24b5484be69112ab6190600da36ffe4619a00903
Anant8002749 <ak8002749@gmail.com>
2021-04-18 21:41:10 +0530

    Added Partials and Layouts

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app=express();
 const port = 8000;
+const expressLayouts=require('express-ejs-layouts');
 
+app.use(express.static('./assets'))
+
+app.use(expressLayouts);
 // use express router
 app.use('/',require('./routes'));
 
