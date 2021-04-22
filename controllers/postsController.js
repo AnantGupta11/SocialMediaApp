7e6565ca54ceda446ff3262f487cbcf9e1f8ea17
Anant8002749 <ak8002749@gmail.com>
2021-04-22 18:02:52 +0530

    Creted Schema for post and saved in db

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,3 +1,14 @@
+const Post=require('../models/post');
+
 module.exports.posts=function(req,res){
-    return res.end('<h1>Posts Controller</h1>')
+    return res.render('home',{
+        title:'Posts'
+    })
+}
+module.exports.createPost=function(req,res){
+    Post.create(req.body,function(err,post){
+        if(err){
+            console.log('Error in creating Post',err);
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 const postsController=require('../controllers/postsController');
 
 router.get('/viewpost',postsController.posts);
+router.post('/create-post',passport.checkAuthentication,postsController.createPost);
 
 
 
