cc7979fadb1f20d9744d6052411b3be19783d50d
Anant8002749 <ak8002749@gmail.com>
2021-04-23 13:54:15 +0530

    Display Post and related User

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,22 @@
+// const { posts } = require("./postsController");
+const Post=require('../models/post');
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('id', 25);
-    return res.render('home',{
-        title: 'Home Page'
+    // console.log(req.cookies);
+    // res.cookie('id', 25);
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: 'Home Page',
+    //         posts:posts
+    //     });
+    // })
+
+    //poppulate the user for each comment
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: 'Social || Home',
+            posts:posts
+        });
     });
+    
+    
 }
