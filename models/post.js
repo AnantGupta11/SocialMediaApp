9993548970fc543b5b3aad05ed5fa6fbee74569a
Anant8002749 <ak8002749@gmail.com>
2021-04-23 15:21:48 +0530

    Creating schema for comments

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema= new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
-    }
+    },
+    //include the array of ids of all comment for each post
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Comment' 
+        }
+    ]
 },{
     timestamps:true
 })
