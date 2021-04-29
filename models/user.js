2e21b28dd6341b6ef0cef11b93fe2448ac5ced31
Anant8002749 <ak8002749@gmail.com>
2021-04-29 15:30:07 +0530

    Configring Multer for User Avatar

--- models/user.js
+++ models/user.js
@@ -17,10 +17,25 @@ const userSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps:true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+   
+// var upload = multer({ storage: storage })
+  
 const User=mongoose.model('User', userSchema);
 
 module.exports=User;
